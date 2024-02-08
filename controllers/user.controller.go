847f23df103c5af5b507c074924e985b4973ee95
deribewsoftware <deribewsoftware@gmail.com>
2024-02-08 12:15:13 +0300

    updated

--- controllers/user.controller.go
+++ controllers/user.controller.go
@@ -1,6 +1,30 @@
 package controllers
 
-func getUsers() {}
+import (
+	"github/gin-gonic/gin"
+
+	"github.com/deribewsoftware/go_jwt/database"
+	"github.com/deribewsoftware/go_jwt/models"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
+var validate = validator.New()
+
+func HashPassword()
+func VerifyPassword(password string)
+
+func getUsers() *gin.HandlerFunc {
+	var user *models.User
+
+	return func(c *gin.Context) {
+		c.Bind
+
+	}
+
+}
 
 func getUserById() {}
 
